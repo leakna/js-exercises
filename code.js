78edfeb86aaed585b5eb053f28fbec6269b36fc4
leakna <phornleakna@gmail.com>
2024-07-09 08:50:09 +0700

    array

--- code.js
+++ code.js
@@ -1,136 +1,64 @@
-
-var fs = require('fs');
 //exercise1
-function squareNumber(number){
-  return number*number
-}
-function map(array,callback){
-  let squareArr=[]
-  array.forEach(element => {
-    squareArr.push(callback(element));
-  });
-  return squareArr
-}
-console.log(map([1,2,3],squareNumber))
+// function filterObj(obj){
+//   let newArr=[]
+//  obj.forEach(element => {
+//     if(element.age>18){
+//       newArr.push(element)
+//     }
+//  });
+//  return newArr
+// }
+// console.log(filterObj(
+//   [
+//     {name:'leakna',age:'18'},
+//     {name:'angel',age:'20'}
+//   ]
+// ))
+
 
 //exercise2
-function filterNumber(number){
-  return (number%2==0)
-}
-function filter(array,callback){
-  let filterArray=[]
-  array.forEach(element => {
-    (callback(element))?filterArray.push(element):null
-  });
-  return filterArray
-}
 
-console.log(filter([1,2,3,4,5],filterNumber))
+// let duplicateArray=[1,1,2,3,4,5,4]
+// let newArray=duplicateArray.filter((element,index,self)=>{
+//   return self.indexOf(element)==index
+  
+// })
+// console.log(newArray)
+
 
 //exercise3
-
-function readFile(filepath){
-  return new Promise((resolve,reject)=>{
-    fs.readFile(filepath,'utf-8',(err,data)=>{
-      if(err)
-        reject(err)
-      else
-        resolve(data)
-    })
-  })
-}
-async function read(filepath,callback){
-  try{
-    return await callback('file/'+filepath)
-  }
-  catch(error){
-    throw error
-  }
-}
-read('fileee.txt',readFile).then(data=>{
-  console.log(data)
-}).catch(err=>{
-  console.log("Error:",err)
-})
+// console.log([
+//   {name:'angel',age:'20'},
+//   {name:'leakna',age:'18'}
+// ].map((element)=>{
+// return {name:element.name};
+// }))
 
 //exercise4
 
-function writeFile(filepath,content){
-  return new Promise((resolve,reject)=>{
-    fs.writeFile(filepath,content,(err,data)=>{
-      if(err)
-        reject(err)
-      else
-        resolve('Writing is successful')
-    })
-  })
-}
-function write(filepath,content,callback){
-  callback('file/'+filepath,content).then((data)=>{console.log(data)})
-  .catch(err=>{
-    console.log(err)
-  })
-}
-write('write.txt','hello world',writeFile)
+// console.log(["2024-01-01", "2024-02-02", "2024-04-04"].map((element)=>{
+//   let date=element.split('-');
+//   return date[2]+'-'+date[1]+'-'+date[0];
+// }))
 
 //exercise5
-function readFile(filepath){
-  return new Promise((resolve,reject)=>{
-    fs.readFile(filepath,'utf-8',(err,data)=>{
-      if(err)
-        reject(err)
-      else
-        resolve(data)
-    })
-  })
-}
-function appendFile(filepath,content){
-  return new Promise((resolve,reject)=>{
-    fs.appendFile(filepath,"\n"+content,(err)=>{
-      if(err)
-        reject(err)
-      else
-        resolve('File is appending')
-    })
-  })
-}
-async function fileOperator(filepath,content,callback){
-  filepath='file/'+filepath
-  if(content){
-      callback(filepath,content).then(message=>{
-          console.log(message)
-      }).catch(err=>{
-        console.log(err)
-      })
+// console.log([1,2,30,4,2,1,11].reduce((accum,current)=>{
+//   if(accum>current)
+//     return accum
+//   else 
+//     return current
+// }))
 
-  }
-   else{
-      
-    try{
-      
-      return await callback(filepath)
-    }
-    catch(err){
-        throw err;
-    }
-   }
-}
 
-(async () => {
-  try {
-    let data = await fileOperator('input.txt', '', readFile);
-    console.log("Data from input.txt:", data);
+//exercise6
 
-    await fileOperator('output1.txt', data, appendFile);
-    data = await fileOperator('output1.txt', '', readFile);
-    console.log("Data from output1.txt:", data);
+// console.log([ {name: "Bread", category: "Grocery"}, 
+//   {name: "Butter", category: "Grocery"},
+//   {name: "Shampoo", category: "Personal Care"}].reduce((accum,current)=>{
+//       if(accum[current.category])
+//         accum[current.category].push(current.name)
+//       else
+//       accum[current.category]=[current.name]
+//     return accum
 
-    await fileOperator('output2.txt', data, appendFile);
-    data = await fileOperator('output2.txt', '', readFile); // This line will throw an error
-    console.log("Data from output2.txt:", data);
-
-  } catch (err) {
-    console.error('catch:', err);
-  }
-})();
-
+//   },{}))
